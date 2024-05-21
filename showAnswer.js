1bf2c23559b8911d9e2687d561cfecc8baac9f86
jasonhe54 <68795646+jasonhe54@users.noreply.github.com>
2024-05-21 07:50:27 -0400

    Update showAnswer.js

--- showAnswer.js
+++ showAnswer.js
@@ -1,8 +1,10 @@
 document.getElementById('showAnswer').addEventListener('click', function() {
     let answerDIV = document.getElementById('answer');
     let answerBTN = document.getElementById('showAnswer');
+    let answerInNewTab = document.getElementById('imgURLOpenInNewTabAnswer');
     let questionDIV = document.getElementById('question');
     answerDIV.classList.remove('d-none');
     answerBTN.classList.add('d-none');
     questionDIV.classList.add('d-none');
+    answerInNewTab.classList.remove('d-none');
 });
